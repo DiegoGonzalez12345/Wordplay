4536cf4a4dabca6b7efac2f4814ca4786f1ef53d
Diego Gonzalez <45858@psdschool.org>
2021-10-28 10:08:39 -0600

    MVP

--- Wordplay/ViewController.swift
+++ Wordplay/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    @IBAction func pressHere(_ sender: Any)
+    {
+        //let nextViewController = mySegue.destination as! Wordplay
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+//        let nextViewController = segue.destination as! WordPlay
+        
+        let viewControllerConst = segue.destination as! WordPlay
+        var myWord2 = textField.text
+        viewControllerConst.newTextField=myWord2 ?? "Enter"
+    }
+    
 }
 
